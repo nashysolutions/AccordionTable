10f91eb9b62900d555d503848fcb8093302a68b3
Rob Nash <r.nash1@me.com>
2022-01-20 09:07:02 +0000

    Attempt to fix UITests in CI.

--- Groceries/Cell.swift
+++ Groceries/Cell.swift
@@ -1,6 +1,11 @@
 import UIKit
 
 final class Cell: UITableViewCell {
+        
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        isAccessibilityElement = true
+    }
 
     func update(with food: Item) {
         let title = food.title
