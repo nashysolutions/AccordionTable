a70546ab81cc43c6af19b462219c30e18666d89d
Rob Nash <r.nash1@me.com>
2022-01-20 05:52:00 +0000

    Update inline documentation.

--- Groceries/Model.swift
+++ Groceries/Model.swift
@@ -1,6 +1,7 @@
 import Foundation
 import OrderedCollections
 
+// Identifiers not needed here as we are in complete control of the data, which is deliberately unique.
 struct Food: Hashable {
     let title: String
     let items: [Item]
